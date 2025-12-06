Fixed errors in both E and M steps of sLDA (batch VI mode): avoiding overflow/underflow issues and mistakes in the method of computing ELBO.

.ipynb_checkpoints/text_processing_utils-checkpoint.py
@@ -76,4 +76,5 @@ def create_bag_of_words(corpus, df_min, df_max_proportion, tfidf_threshold_q, to
             del word_counts_by_document[k][k2]
     return word_counts_by_document, vocab_new, n
 
-self.word_indices = {d:np.hstack([[k]*v for k,v in bag.items()]) for d,bag in enumerate(bow)} # convert the dictionary of word counts into an array
+def convert_bow(bow):
+    return {d:np.hstack([[k]*v for k,v in bag.items()]) for d,bag in enumerate(bow)} # convert the dictionary of word counts into an array

variational_inference_utils.py
@@ -4,8 +4,11 @@ def normalize_vector(arr):
     arr_sum = np.sum(arr)
     return arr / arr_sum
 
-def normalize_by_row(arr):
-    return (arr.T / arr.sum(axis=1)).T
+def exp_normalize_by_row(arr):
+    # exponentiate, and then normalize to 1 by each row (using the idea of log-sum-exp)
+    arr_rowmax = arr.max(axis=1)
+    arr_new = np.exp(arr.T - arr_rowmax)
+    return (arr_new / arr_new.sum(axis=0)).T
 
 def exp_normalize(arr):
     # exponentiate, and then normalize to 1 (using the idea of log-sum-exp)
@@ -27,3 +30,7 @@ def stochastic_variational_update(old_value, value_hat, rho):
 def stochastic_hyperparameter_update(old_value, value_hat, rho):
     new_value = old_value - rho * value_hat
     return new_value
+
+def create_gamma_matrix():
+    # convert gamma into matrix format, which is required in M step
+    pass